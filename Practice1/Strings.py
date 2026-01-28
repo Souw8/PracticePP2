77fa9c5ac422441630dd9a923b846b61c39e3754
Souw8 <godkana456@gmail.com>
2026-01-28 12:09:46 +0500

    new

--- Practice1/Strings.py
+++ Practice1/Strings.py
@@ -20,3 +20,27 @@ for x in "banana":
 
 txt = "The best things in life are free!"
 print("free" in txt) #you can check if in this sentence  we need to check word free in this senntence and output will be true
+
+
+b = "Hello, World!"
+print(b[2:5]) #with this  we can retrieve elements of string  with index dia.
+
+
+a = "Hello, World!"
+print(a.upper())
+a = "Hello, World!"
+print(a.lower())
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+txt = "We are the so-called \"Vikings\" from the north."
