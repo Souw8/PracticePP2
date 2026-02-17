f9a280db6f90bd23f7fdf06936068f8d7d86d310
Souw8 <godkana456@gmail.com>
2026-02-18 00:09:50 +0500

    Lambda is complete

---
+++ Practice3/Lambda/lambda_basics.py
@@ -0,0 +1,7 @@
+#lambda arguments : expression syntaxis of lambda
+x = lambda a : a + 10
+print(x(5))
+#Multiply argument a with argument b and return the result:
+
+x = lambda a, b : a * b
+print(x(5, 6))
