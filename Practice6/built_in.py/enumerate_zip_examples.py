b67af049cfe6733993e812726672a78d3e072258
Souw8 <godkana456@gmail.com>
2026-03-18 12:46:07 +0500

    built in

---
+++ Practice6/built_in.py/enumerate_zip_examples.py
@@ -0,0 +1,9 @@
+names = ["a", "b", "c"]
+
+for i, name in enumerate(names):
+    print(i, name)
+
+a = [1,2]
+b = [3,4]
+
+print(list(zip(a, b)))
---
+++ Practice6/built_in.py/map_filter_reduce.py
@@ -0,0 +1,4 @@
+nums = [1, 2, 3]
+
+print(list(map(lambda x: x*2, nums)))
+print(list(filter(lambda x: x > 1, nums)))
